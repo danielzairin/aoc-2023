50e9006d7310ff02bd9418c77ba9f3e6659858be
Daniel Zairin <danielzairink@gmail.com>
2024-04-03 11:02:28 +0800

    day 3 part 2

--- day-3/main.go
+++ day-3/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
@@ -12,7 +13,19 @@ func main() {
 		panic(err)
 	}
 
-	partOne(string(file))
+	result, err := partOne(string(file))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("part 1 = %d\n", result)
+
+	result, err = partTwo(string(file))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("part 2 = %d\n", result)
 }
 
 type position [2]int
@@ -40,21 +53,71 @@ func partOne(file string) (int, error) {
 				x++
 			}
 			if s != "" {
+				num, err := strconv.Atoi(line[start:x])
+				if err != nil {
+					return 0, err
+				}
+
 			findAdjacentSymbol:
 				for i := y - 1; i <= y+1; i++ {
 					for j := start - 1; j < x+1; j++ {
 						_, ok := symbols[position{j, i}]
 						if ok {
-							parsed, err := strconv.Atoi(line[start:x])
-							if err != nil {
-								return 0, err
-							}
-							result += parsed
+							result += num
 							break findAdjacentSymbol
 						}
 					}
 				}
 			}
+		}
+	}
+
+	return result, nil
+}
+
+func partTwo(file string) (int, error) {
+	lines := strings.Split(file, "\n")
+	hashmap := make(map[position][]int)
+
+	for y, line := range lines {
+		for x, char := range line {
+			if char == '*' {
+				hashmap[position{x, y}] = make([]int, 0)
+			}
+		}
+	}
+
+	for y, line := range lines {
+		for x := 0; x < len(line); x++ {
+			var s string
+			start := x
+			for x < len(line) && isDigit(rune(line[x])) {
+				s += string(line[x])
+				x++
+			}
+			if s != "" {
+				num, err := strconv.Atoi(line[start:x])
+				if err != nil {
+					return 0, err
+				}
+
+				for i := y - 1; i <= y+1; i++ {
+					for j := start - 1; j < x+1; j++ {
+						_, ok := hashmap[position{j, i}]
+						if ok {
+							hashmap[position{j, i}] = append(hashmap[position{j, i}], num)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	result := 0
+
+	for _, nums := range hashmap {
+		if len(nums) == 2 {
+			result += nums[0] * nums[1]
 		}
 	}
 
