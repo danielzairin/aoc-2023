f157074706d4cd248110938bf28c8ff644fb0fae
Daniel Zairin <danielzairink@gmail.com>
2024-04-17 19:27:38 +0800

    day 4 part 2

--- day-4/main.py
+++ day-4/main.py
@@ -21,4 +21,27 @@ def part1(lines):
     
     print(f"part 1 = {result}")
 
+def part2(lines):
+    result = len(lines)
+    memo = {}
+
+    for i in range(len(lines) - 1, -1, -1):
+        _, body = lines[i].split(': ')
+        strWinners, strNums = body.split('|')
+        winners = set(strWinners.split())
+
+        count = 0
+        for num in strNums.split():
+            if num in winners:
+                count += 1
+        
+        for j in range(count):
+            count += memo[i + j + 1]
+        
+        memo[i] = count
+        result += count
+    
+    print(f"part 2 = {result}")
+
 part1(lines)
+part2(lines)
