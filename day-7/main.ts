310e7a43996684a7db5b36f8753870ad7c85ceb1
Daniel Zairin <danielzairink@gmail.com>
2024-04-30 09:53:36 +0800

    day 7 part 2

--- day-7/main.ts
+++ day-7/main.ts
@@ -1,5 +1,6 @@
 import fs from "node:fs";
 import { compareHands } from "./lib";
+import { compareHandsP2 } from "./lib2";
 
 function part1() {
   const file = fs.readFileSync("input.txt", "utf-8");
@@ -21,4 +22,25 @@ function part1() {
   console.log(`part 1 = ${result}`);
 }
 
+function part2() {
+  const file = fs.readFileSync("input.txt", "utf-8");
+  const inputs: { hand: string; bid: number }[] = [];
+
+  for (const line of file.split("\n")) {
+    const [hand, bid] = line.split(" ");
+    inputs.push({ hand, bid: Number.parseInt(bid) });
+  }
+
+  inputs.sort((a, b) => -compareHandsP2(a.hand, b.hand));
+
+  let result = 0;
+
+  for (let i = 0; i < inputs.length; i++) {
+    result += inputs[i].bid * (i + 1);
+  }
+
+  console.log(`part 2 = ${result}`);
+}
+
 part1();
+part2();
